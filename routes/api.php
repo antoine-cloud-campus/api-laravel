651f3857440ab10dfc9e8108cde23540fb9be962
Nubunduram <ceantoine@hotmail.fr>
2026-02-10 15:15:21 +0100

    refactor: routes match put/patch

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [UserController::class, 'logout']);
 
     Route::post('/books', [BookController::class, 'store']);
-    Route::put('/books/{book}', [BookController::class, 'update']);
-    Route::patch('/books/{book}', [BookController::class, 'update']);
+    Route::match(['put', 'patch'], '/books/{book}', [BookController::class, 'update']);
     Route::delete('/books/{book}', [BookController::class, 'destroy']);
 });
