3db3d7acd0e9f9dca07d4eb36ffff5a2ba15f3c9
Nubunduram <ceantoine@hotmail.fr>
2026-02-11 11:01:56 +0100

    fix ex 5

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -8,13 +8,15 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BookResource extends JsonResource
 {
-    
+
     public function toArray($request)
     {
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'author' => $this->author,
+            'author' => mb_strtoupper($this->author),
+            'summary' => $this->summary,
+            'isbn' => $this->isbn,
             'created_at' => $this->created_at->format('d/m/Y'),
 
             '_links' => [
